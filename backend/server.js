c3da5cbc83db847f62ad8fcc2cf83e5ac8f0cf7b
msanjana11 <sanjanamanogaran11@gmail.com>
2026-05-10 23:28:32 +0530

    update backend deployment fixes and frontend API

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ require("dotenv").config({ path: ".env" });
 const app = express();
 
 // middleware
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000", "https://your-vercel-url.vercel.app"]
+}));
+
 app.use(express.json());
 
 // routes
@@ -27,10 +31,11 @@ app.get("/", (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("MongoDB connected");
+const PORT = process.env.PORT || 5000;
 
-    app.listen(5000, () => {
-        console.log("Server running on port 5000");
-    });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 })
 .catch((err) => {
     console.log("MongoDB connection error:", err);
